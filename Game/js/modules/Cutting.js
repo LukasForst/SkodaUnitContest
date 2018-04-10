c4c343cab3ce1ea3dc02b46d7846d75384c12260
msklenar <matto.sklenar@gmail.com>
2018-04-10 13:29:40 +0200

    WIP

--- Game/js/modules/Cutting.js
+++ Game/js/modules/Cutting.js
@@ -1,103 +1,32 @@
-context = document.getElementById('cuttingStage').getContext("2d");
+class Cutting{
+    metal_sheet;
+    component_tab;
+    pressButton;
 
-$('#canvas').mousedown(function(e){
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
+    constructor() {
+        this.metal_sheet = document.querySelector('.metal_sheet');
+        this.component_tab = document.querySelector('.component_tab');
+        this.pressButton = document.querySelector('.pressButton');
 
-    paint = true;
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    redraw();
-});
-
-$('#canvas').ontouchstart(function(e){
-    var mouseX = e.pageX - this.offsetLeft;
-    var mouseY = e.pageY - this.offsetTop;
-
-    paint = true;
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-    redraw();
-});
-
-$('#canvas').mousemove(function(e){
-    if(paint){
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-        redraw();
+        metal_sheet.addEventListener('click', function (e) {
+            metal_sheet.classList.add('hidden');
+            createComponentTab();
+        })
     }
-});
 
+    createComponentTab() {
+        this.pressButton.classList.remove('hidden');
+        this.pressButton.classList.remove('hidden');
+        var img = component_tab.createElement('div');
+        var btn = component_tab.createElement('button');
 
-$('#canvas').touchmove(function(e){
-    if(paint){
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-        redraw();
+        img.classList.add('door');
+        btn.innerHTML = 'Lisovat';
+        btn.addEventListener('click', function (ev) {
+            if(!img.classList.contains('pressed')){
+                img.classList.add('pressed');
+            }
+        })
     }
-});
 
-
-
-// $('#canvas').touchstart(function(e){
-//     paint = false;
-// });
-
-$('#canvas').mouseup(function(e){
-    paint = false;
-});
-
-$('#canvas').touchend(function(e){
-    paint = false;
-});
-
-$('#canvas').mouseleave(function(e){
-    paint = false;
-});
-
-$('#canvas').touchcancel(function(e){
-    paint = false;
-});
-
-
-var clickX = new Array();
-var clickY = new Array();
-var clickDrag = new Array();
-var paint;
-
-function redraw(){
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-
-    context.strokeStyle = "#17df00";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-
-    for(var i=0; i < clickX.length; i++) {
-        context.beginPath();
-        if(clickDrag[i] && i){
-            context.moveTo(clickX[i-1], clickY[i-1]);
-        }else{
-            context.moveTo(clickX[i]-1, clickY[i]);
-        }
-        context.lineTo(clickX[i], clickY[i]);
-        context.closePath();
-        context.stroke();
-    }
 }
-
-function gameOver() {
-    alert("Prohral jsi");
-}
-
-var x = document.querySelector('.component');
-x.addEventListener('click',function(){
-        gameOver();
-    });
-
-
-
-function addClick(x, y, dragging)
-{
-    clickX.push(x);
-    clickY.push(y);
-    clickDrag.push(dragging);
-}
-
-
-
